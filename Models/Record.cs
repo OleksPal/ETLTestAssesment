50475641abed18541e766b70e228c399b6373e74
User <oleksandr.palamarchuk1310@gmail.com>
2025-01-02 21:54:30 +0200

    Add the record model

---
+++ Models/Record.cs
@@ -0,0 +1,16 @@
+﻿namespace ETLTestAssesment.Models
+{
+    public class Record
+    {
+        public int Id { get; set; }
+        public DateTime TpepPickupDatetime { get; set; }
+        public DateTime TpepDropoffDatetime { get; set; }
+        public int PassengerCount { get; set; }
+        public double TripDistance { get; set; }
+        public bool StoreAndFwdFlag { get; set; }
+        public int PULocationID { get; set; }
+        public int DOLocationID { get; set; }
+        public decimal FareAmount { get; set; }
+        public decimal TipAmount { get; set; }
+    }
+}
