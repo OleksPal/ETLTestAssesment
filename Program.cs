f9113bc6d74b514230929090bc247be53822119d
User <oleksandr.palamarchuk1310@gmail.com>
2025-01-02 21:38:21 +0200

    Add NuGet packages and create empty ETL process definition

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
-﻿namespace ETLTestAssesment
+﻿using Paillave.Etl.Core;
+
+namespace ETLTestAssesment
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var processRunner = StreamProcessRunner.Create<string>(DefineProcess);
+            var res = await processRunner.ExecuteAsync(args[0]);
+            Console.Write(res.Failed ? "Failed" : "Succeeded");
+        }
+
+        private static void DefineProcess(ISingleStream<string> contextStream)
+        {
+            // TODO: Define the ETL process here
         }
     }
 }
